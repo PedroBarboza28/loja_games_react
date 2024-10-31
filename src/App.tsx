9eff9d5087604d8f02caf6d23194395e46993ea3
Pedro Barboza <peh123barboza@gmail.com>
2024-10-30 23:26:32 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/home/Home";
 import { Navbar } from "./components/navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MenuProvider } from "./context/Context";
-import Flight from "./components/gamedetail/Flight";
+import Flight from "./components/gameDetail/Flight";
 
 function App() {
   return (
